23becbdb4b4beedef6266408f2dbff65583a4448
Gabriel Agbo <agbogabrielc@gmail.com>
2024-01-17 09:06:52 +0000

    API update

--- wolfandbadger/urls.py
+++ wolfandbadger/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('wbapp.urls')),
+    path("api/", include('api.urls')),
     path('accounts/', include('allauth.urls')),
 ]
