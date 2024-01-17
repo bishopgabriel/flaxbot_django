002e512729cfdbd7f6275de35dff1473d7f53ef3
Gabriel Agbo <agbogabrielc@gmail.com>
2024-01-17 08:36:22 +0000

    first migration

--- wbapp/models.py
+++ wbapp/models.py
@@ -3,4 +3,15 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    pass
+    username = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    created = models.DateField(null=True)
+    updated = models.DateTimeField(auto_now=True)  #auto_now_add=True
+
+    def __str__(self) -> str:
+        return self.username
+    # class Meta:
+    #     db_table = "wbapp_userprofile"
+    #     indexes = models.Index(fields=['username'])
