36e3f817380ffcbe993a80706ceb9781841c31fb
Gabriel Agbo <agbogabrielc@gmail.com>
2024-01-17 08:57:45 +0000

    forms
    Updated the forms

--- wbapp/forms.py
+++ wbapp/forms.py
@@ -7,7 +7,7 @@ class UserProfileForm(forms.ModelForm):
         fields = ['username', 'address', 'phone', 'email', 'created']  # You can exclude 'created' if you don't want users to modify it
         widgets = {
             'created': forms.HiddenInput(),
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
             'address': forms.TextInput(attrs={'class': 'form-control'}),
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
@@ -18,3 +18,9 @@ class UserProfileForm(forms.ModelForm):
         if UserProfile.objects.exclude(id=self.instance.id).filter(email=email).exists():
             raise forms.ValidationError('This email address is already in use.')
         return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if UserProfile.objects.exclude(id=self.instance.id).filter(username=username).exists():
+            raise forms.ValidationError('This username is already in use.')
+        return username
